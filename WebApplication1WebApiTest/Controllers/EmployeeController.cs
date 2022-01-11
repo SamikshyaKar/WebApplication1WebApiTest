336a72ce860d19e09e8e5329a18f098ccaf0bb74
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-12 00:20:27 +0530

    first commit

---
+++ WebApplication1WebApiTest/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1WebApiTest.Models;
+
+namespace WebApplication1WebApiTest.Controllers
+{
+
+   
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController:ControllerBase
+
+    {
+        private readonly IEmployeerepository employeerepository;
+
+        public EmployeeController(IEmployeerepository iemployeeRepository)
+        {
+            this.employeerepository = iemployeeRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllEmployees()
+        {
+            try
+            {
+                return Ok(await employeerepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,"error rzetrieving data from database");
+            }
+            
+        }
+
+
+
+
+    }
+}
