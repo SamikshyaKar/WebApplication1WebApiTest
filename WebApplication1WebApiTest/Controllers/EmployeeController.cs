56f03767044014d511b6d942074bd739442b0997
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-12 00:50:47 +0530

    --Second commit

--- WebApplication1WebApiTest/Controllers/EmployeeController.cs
+++ WebApplication1WebApiTest/Controllers/EmployeeController.cs
@@ -38,6 +38,31 @@ namespace WebApplication1WebApiTest.Controllers
         }
 
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployeebyID(int ID)
+        {
+            try
+            {
+                var result= (await employeerepository.GetEmployeebyID(ID));
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,"Error retrieving data from database");
+
+            }
+
+        }
+
+
 
 
     }
